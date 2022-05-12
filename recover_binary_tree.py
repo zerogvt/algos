070c6203dcb41a84108feb1d8444e4a7852131e6
Vasilis <12730010+zerogvt@users.noreply.github.com>
2022-05-12 15:00:37 +0300

    Create recover_binary_tree.py

---
+++ recover_binary_tree.py
@@ -0,0 +1,54 @@
+# You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree 
+# were swapped by mistake. Recover the tree without changing its structure.
+
+#Solution => inorder traversal of BST should gives a sorted array in ascending order.
+#  then find the 2 swapped elements in that array
+#  then go into a new traversal changing the swapped elements 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inOrder(self, root: TreeNode) -> List[int]:
+        return self.inOrder(root.left) + [root.val] + self.inOrder(root.right) if root else []
+
+    def inOrderAndSwap(self, root: TreeNode, swapped: List[int]) -> None:
+        if not root:
+            return
+        if root.val == swapped[0]:
+            root.val = swapped[1]
+        elif root.val == swapped[1]:
+            root.val = swapped[0]
+        self.inOrderAndSwap(root.left, swapped)
+        self.inOrderAndSwap(root.right, swapped)
+    
+    def findTwoSwapped(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        x = y = None # Initialize x and y as a value that cannot be the value of a node.
+        for i in range(n - 1):
+            if nums[i + 1] < nums[i]:
+                y = nums[i + 1]
+                # first swap occurrence
+                if x is None:     
+                    x = nums[i]
+                # second swap occurrence
+                else:           
+                    break
+        return [x, y]
+        
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        nums = self.inOrder(root)
+        print(nums)
+        swapped = self.findTwoSwapped(nums)
+        print(swapped)
+        self.inOrderAndSwap(root, swapped)
+        print(self.findTwoSwapped([1, 2, 3, 10, 7, 8, 4]))
+        
